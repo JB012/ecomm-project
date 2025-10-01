16837c944279a0e419e9c3e589ab9858718a3309
Jamal Bankole <jbankole@terpmail.umd.edu>
2025-10-01 13:43:26 -0400

    Changed link URLs

--- app/components/HeaderPhoto.tsx
+++ app/components/HeaderPhoto.tsx
@@ -1,5 +1,6 @@
 "use client"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function HeaderPhoto() {
 
@@ -8,7 +9,7 @@ export default function HeaderPhoto() {
             <div className="flex h-full flex-col text-black w-full px-12 gap-6 z-10 font-medium justify-center absolute">
                 <div className="text-7xl">Shop Now!</div>
                 <div className="text-5xl">Order the latest items with hot deals</div>
-                <Link href={}><button className="self-end">Shop All</button></Link>
+                <Link href={`${window.location.origin}/viewProducts?category=all&sort=asc_name`}><button className="self-end">Shop All</button></Link>
             </div>
             <Image src={`/headerImage.jpg`} className="opacity-75" alt={'Header Image'} width={0} height={0} sizes="100vw" style={{width: "100%", height: "100%", objectFit: 'cover'}}/>
         </div>
